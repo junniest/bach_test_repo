3899b1d1eafd7bd55032c7e6320aaf87021000be
June Pecherskaya <naijulejshaja@gmail.com>
2012-04-22 15:21:47 +0300

    refactoring, part 1

--- nfa.py
+++ nfa.py
@@ -2,10 +2,15 @@ __author__ = "Artem Shinkarov, June Pecherskaya"
 __date__ = "2012-02-09"
 
 import collections
+import time
 
-# Abstract class for Nondeterminate Finite Automaton
+
+""" ==== Nondeterminate Finite Automaton logic ==== """
+
+""" ---- Abstract class for NFA ---- """
+
 class nfa (object):
-    "NFA class for regular expressions"
+    """ NFA class for regular expressions. """
     def __init__ (self):
         self.start = None
         self.end = None
@@ -14,32 +19,55 @@ class nfa (object):
         self.parent = None
 
     def add_next_state (self, state):
+        """ Adds a next state. """
         if self.start is None or self.end is None:
             raise Exception ("Abstract nfa instantiated")
         else:
             self.end.nxt = state.start
             self.end = state.end
         return self
-    
+
+    def get_next_state(self):
+        """ Returns the next state for an NFA state. """
+        if self.nxt is not None:
+            return self.nxt
+        if isinstance (self.parent, or_nfa):
+            return self.parent.get_next_state ()
+        if isinstance (self.parent, asterisk_nfa):
+            if isinstance (self.parent.parent, asterisk_nfa):
+                return self.parent.get_next_state()
+            else:
+                return self.parent
+        return None
+
     def __repr__ (self):
         return "<abstract nfa node>"
 
 
-# Subclasses of NFA
+""" ---- Subclasses of NFA ---- """
+
 class char_nfa (nfa):
+    """ NFA class for matching a single charachter. """
     def __init__ (self, c):
         nfa.__init__ (self)
         self.character = c
         self.start = self
         self.end = self
 
+    def get_epsilon_closure (self, closure):
+        """ Epsilon closure for a single state. """
+        if not self in closure:
+            closure.add (self)
+    
     def __repr__ (self):
         str = "char[%s]" % (self.character) 
         if self.nxt is not None:
             str = str + " -> " + repr (self.nxt)
         return str
 
-class asterix_nfa (nfa):
+
+class asterisk_nfa (nfa):
+    """ NFA class for matching an asterisk group. """
     def __init__ (self, state):
         nfa.__init__ (self)
         self.content = state
@@ -47,6 +75,13 @@ class asterix_nfa (nfa):
         self.end = self
         state.end.parent = self
 
+    def get_epsilon_closure (self, closure):
+        """ Epsilon closure for a single state. """
+        if not self in closure:
+            closure.add (self)
+            self.content.get_epsilon_closure (closure)
+            self.get_next_state ().get_epsilon_closure (closure)
+    
     def __repr__ (self):
         str = "( %s )*" % repr (self.content)
         if self.nxt is not None:
@@ -55,6 +90,7 @@ class asterix_nfa (nfa):
 
 
 class or_nfa (nfa):
+    """ NFA class for matching one of two alternatives. """
     def __init__ (self, state0, state1):
         nfa.__init__ (self)
         self.alternative = (state0, state1)
@@ -62,6 +98,13 @@ class or_nfa (nfa):
         self.end = self
         state0.end.parent = self
         state1.end.parent = self
+
+    def get_epsilon_closure (self, closure):
+        """ Epsilon closure for a single state. """
+        if not self in closure:
+            closure.add (self)
+            self.alternative[0].get_epsilon_closure (closure)
+            self.alternative[1].get_epsilon_closure (closure)
     
     def __repr__ (self):
         str = "( %s | %s )" % (repr (self.alternative[0]), repr (self.alternative[1]))
@@ -71,10 +114,16 @@ class or_nfa (nfa):
 
 
 class done_nfa (nfa):
+    """ NFA class for the finishing state of the automaton """
     def __init__ (self):
         nfa.__init__ (self)
         self.start = self
         self.end = self
+
+    def get_epsilon_closure (self, closure):
+        """ Epsilon closure for a single state. """
+        if not self in closure:
+            closure.add (self)
 
     def __repr__ (self):
         if self.start.nxt is not None:
@@ -82,119 +131,95 @@ class done_nfa (nfa):
         return "[Done]"
 
 
-# Class for Determinate Finate Automaton
+""" ==== Determinate Finite Automaton logic ==== """
+
+""" ---- DFA node class ---- """
+
 class node_dfa (object):
-    "DFA node class"
+    """ DFA node class. """
     def __init__ (self, nfa_lst):
         self.states = nfa_lst
         self.paths = {}
         self.accepting = False
         self.regexp_id = None
   
+    def get_moves (self, symbol):
+        """ Returns moves possible from the given DFA state (NFA state list)
+            by a specific symbol. """
+        moves = []
+        for nfa_state in self.states:
+            if isinstance (nfa_state, char_nfa):
+                if nfa_state.character == symbol:
+                    moves.append (nfa_state.get_next_state ())
+        return moves
+    
     def __repr__ (self):
 #        state_list = map(lambda (x,y): x + ' -> ' + repr(y.id), self.paths.iteritems())
         return "<id:%s, accept:%r>" % (self.id, self.accepting)#, state_list)
 
+""" ---- Logic for DFA creation from an NFA ---- """
 
-def add_to_state_list (lst, dfa_lst):
-    "Construct dfa from nfa states LST. In case dfa is not in\
-     in the list -- add it.  Return dfa."
-    dfa = node_dfa (lst)
-    
-    l = filter (lambda x: set (x.states) == set (dfa.states), dfa_lst)
+def add_to_state_list (nfa_lst, dfa_lst):
+    """ Locate a DFA state in the list using the given the list of NFA states.
+        If such a DFA state is present, return it. If it is not present, create
+        a new state, add it to the DFA state list and return it. If two or more
+        such states are present in the list - raise an exception. """
+    l = filter (lambda x: set (x.states) == set (nfa_lst), dfa_lst)
     if len (l) == 1:
         dfa = l[0];
     elif len (l) == 0:
+        dfa = node_dfa (nfa_lst)
         dfa_lst.append (dfa)
     else:
-        raise Exception ("duplicate dfa with id %r found" % dfa.states)
-
+        raise Exception ("Duplicate DFA state with id %r found" % dfa.states)
     return dfa
 
 
-def get_next_state(state):
-    if state.nxt is not None:
-        return state.nxt
-    if isinstance (state.parent, or_nfa):
-        return get_next_state(state.parent)
-
-    # FIXME Looks really suspicious.  What if we have more than
-    # two levels of nesting in asterix?
-    if isinstance (state.parent, asterix_nfa):
-        if isinstance (state.parent.parent, asterix_nfa):
-            return get_next_state(state.parent)
-        else:
-            return state.parent
-    return None
-
-
-def get_moves (dfa_state, symbol):
-    "Returns moves possible from the given dfa \
-     state (nfa state list) by a specific symbol"
-    moves = []
-    for nfa_state in dfa_state.states:
-        if isinstance (nfa_state, char_nfa):
-            if nfa_state.character == symbol:
-                moves.append (get_next_state(nfa_state))
-    return moves
-
-
 def get_epsilon_closure (nfa_state_list):
-    "Epsilon closure for a list of states"
-    closure = []
-    closure_one = []
-    
+    """ Method gets the epsilon closure for a list of states. """
+    closure = set()
     for state in nfa_state_list:
-        get_epsilon_closure_single (state, closure_one)
-        if closure_one:
-            closure = closure + closure_one
+        state.get_epsilon_closure (closure)
     if closure:
         return closure
     return None
-    
-
-def get_epsilon_closure_single (automata, closure):
-    "Epsilon closure for a single state"
-    if not automata is None and not automata in closure:
-        if isinstance (automata, char_nfa) or isinstance (automata, done_nfa):
-            closure.append (automata)
-        elif isinstance (automata, asterix_nfa):
-            closure.append (automata)
-            get_epsilon_closure_single (automata.content, closure)
-            get_epsilon_closure_single (get_next_state(automata), closure)
-        elif isinstance (automata, or_nfa):
-            closure.append (automata)
-            get_epsilon_closure_single (automata.alternative[0], closure)
-            get_epsilon_closure_single (automata.alternative[1], closure)
 
 
 def rearrange (dfa_state_list):
-    for state in dfa_state_list:
-        for nfa_state in state.states:
-            if isinstance (nfa_state, done_nfa):
-                state.accepting = True
+    """ Method sets DFA state "accepting" parameter according to the NFA states 
+        it contains. Then sets an id for the state and removes unnecessary 
+        information (the list of NFA states). """
     for i in xrange(len(dfa_state_list)):
+        dfa_state_list[i].accepting = \
+            not not filter(lambda nfa_state: isinstance (nfa_state, done_nfa), 
+                           dfa_state_list[i].states)
         dfa_state_list[i].id = i
+        dfa_state_list[i].states = None
     return dfa_state_list
 
-def det (automata, symbol_list):
+
+def det (automaton, symbol_list):
+    """ Method creates a DFA for the given NFA using its starting state and a 
+        list of symbols that are used in it for traversing the automaton. """
     dfa_state_list = []
-    nfa_state_list = get_epsilon_closure ([automata])
+    nfa_state_list = get_epsilon_closure ([automaton])
     if not nfa_state_list:
-        raise Exception ("No states were found for %s" % automata)
+        raise Exception ("No states were found for %s" % automaton)
         
     add_to_state_list (nfa_state_list, dfa_state_list)
     
     for dfa in dfa_state_list:
         for symbol in symbol_list:
-            nl = get_epsilon_closure (get_moves (dfa, symbol))
+            nl = get_epsilon_closure (dfa.get_moves (symbol))
             if nl:
                dfa.paths[symbol] = add_to_state_list (nl, dfa_state_list)
     return minimize (rearrange (dfa_state_list))
 
 
+""" ---- Logic for DFA minimization ---- """
+
 def get_group_no(state, group_list):
-    "Gets the group number which contains the state in question"
+    """ Gets the group number which contains the state in question """
     for i in xrange (len (group_list)):
         if state in group_list[i]:
             return i
@@ -202,7 +227,8 @@ def get_group_no(state, group_list):
 
 
 def break_to_groups (group, group_list):
-    "Breaks the given group into smaller groups according to the paths they have"
+    """ Breaks the given group into smaller groups according to the paths they 
+        have. """
     result = []
     state_path_list = []
     # the cycle creates pairs of states and their paths encoded as paths to a 
@@ -228,8 +254,9 @@ def break_to_groups (group, group_list):
 
 
 def minimize (automata):
-    "This method minimizes the automata"
-    # starting breakdown - finishing and unfinishing states are separated
+    """ This method minimizes the automata """
+    # starting breakdown - accepting and not accepting states are separated into
+    # two groups.
     group_list = [[],[]]
     for state in automata:
         if state.accepting:
@@ -276,8 +303,11 @@ def make_automata_from_groups (group_list):
     automata.insert (0, start_state)
     return automata
 
+
+""" ==== Regular expression parse logic ==== """
+
 class getter (object):
-    "Getter class for the string"
+    """ Character getter class for regular expression. """
     def __init__ (self, string):
         self.string = string
         self.ind = 0
@@ -291,118 +321,79 @@ class getter (object):
     
     def unget (self):
         self.ind = self.ind - 1
-        
-    def eof (self):
-        if self.ind == len(self.string):
-            return True
-        return False
 
-def concat (stack, seq_len):
-    "If possible, concatenates the last seq_len states of the stack"
-    i = 1
-    while len (stack) > 1 and i < seq_len:
-        state1 = stack.pop()
-        state0 = stack.pop()
-        stack.append (state0.add_next_state (state1))
-        i = i + 1
+class parser (object):
+    """ Regular expression parser class. """
+    def __init__ (self):
+        self.gtr = None
+        self.stack = None
 
-def handle_primary (gtr, stack):
-    "Handles the primary matches - characters and *, calls handle_or for brace content"
-    c = gtr.get_char ();
-    if c is None:
+    def concat (self, seq_len):
+        """ If possible, concatenates the last seq_len states of the stack. """
+        i = 1
+        while len (self.stack) > 1 and i < seq_len:
+            state1 = self.stack.pop()
+            state0 = self.stack.pop()
+            self.stack.append (state0.add_next_state (state1))
+            i = i + 1
+
+    def handle_primary (self):
+        """ Handles the primary matches - characters and *, calls handle_or for
+            brace content. """
+        c = self.gtr.get_char ();
+        if c is None:
+            return
+        if c.isalnum ():
+            self.stack.append (char_nfa (c))
+            return
+        if c == '*':
+            self.stack.append (asterisk_nfa (self.stack.pop ()))
+            return
+        if c == '(':
+            self.handle_or ()
+            c = self.gtr.get_char ()
+            if c != ')':
+                raise Exception ('Closing brace expected, got %s instead.' % c)
+            return
+        self.gtr.unget ()
         return
-    if c.isalnum ():
-        stack.append (char_nfa (c))
+
+    def handle_seq (self):
+        """ Handles primary match sequenes. """
+        self.handle_primary ()
+        seq_len = 1
+        char = self.gtr.get_char ()
+        while char is not None and char != '|' and char != ')':
+            self.gtr.unget ()
+            old_len = len (self.stack)
+            self.handle_primary ()
+            if (old_len != len (self.stack)):
+                seq_len = seq_len + 1
+            char = self.gtr.get_char ()
+        self.concat (seq_len)
+        self.gtr.unget ()
         return
-    if c == '*':
-        stack.append (asterix_nfa (stack.pop ()))
-        return
-    if c == '(':
-        handle_or (gtr, stack)
-        c = gtr.get_char ()
-        if c != ')':
-            raise Exception ('Closing brace expected, got %s instead.' % c)
-        return
-    gtr.unget ()
-    return
 
-def handle_seq (gtr, stack):
-    "Handles primary match sequenes"
-    handle_primary (gtr, stack)
-    seq_len = 1
-    char = gtr.get_char ()
-    while char is not None and char != '|' and char != ')':
-        gtr.unget ()
-        old_len = len (stack)
-        handle_primary (gtr, stack)
-        if (old_len != len (stack)):
-            seq_len = seq_len + 1
-        char = gtr.get_char ()
-    concat (stack, seq_len)
-    gtr.unget ()
-    return
-
-def handle_or (gtr, stack):
-    "Handles or's"
-    handle_seq (gtr, stack)
-    if gtr.get_char () == "|":
-        handle_seq (gtr, stack)
-        state0 = stack.pop ()
-        state1 = stack.pop ()
-        stack.append (or_nfa (state1, state0))
-    else:
-        gtr.unget ()
-    
-def parse (string):
-    gtr = getter (string)
-    stack = []
-    handle_or (gtr, stack)
-    if not stack:
-        raise Exception ('Empty regexp!')
-    stack[0].add_next_state (done_nfa ())
-    return stack[0]
-
-
-def execute (string, regexp_list):
-    automata_list = [det(parse(x), ''.join([c for c in set(x) if c not in '*|()'])) for x in regexp_list]
-    for i in xrange (len (automata_list)):
-        for state in automata_list[i]:
-            state.regexp_id = i
-    automata = merge (automata_list)
-    current_state = automata[0]
-    fail = False
-    for char in string:
-        if current_state.paths.has_key (char):
-            current_state = current_state.paths.get (char)
+    def handle_or (self):
+        """ Handles or's. """
+        self.handle_seq ()
+        if self.gtr.get_char () == "|":
+            self.handle_seq ()
+            state0 = self.stack.pop ()
+            state1 = self.stack.pop ()
+            self.stack.append (or_nfa (state1, state0))
         else:
-            fail = True
-            break
-    if fail:
-        print "Nothing accepted."
-    else:
-        accepted = filter(lambda x: x.accepting, current_state.state_list)
-        if not accepted:
-            print "Nothing accepted."
-        else:
-            print "Accepted", regexp_list[accepted[0].regexp_id]
+            self.gtr.unget ()
 
-
-#    auto_dict = dict (zip (xrange (len(automata_list)),\
-#			   [auto[0] for auto in automata_list]))
-#    for char in string:
-#        to_throw_out = []
-#        for key in auto_dict.iterkeys ():
-#            if auto_dict.get (key).paths.has_key (char):
-#                auto_dict[key] = auto_dict.get (key).paths.get (char)
-#            else:
-#                to_throw_out.append (key)
-#	pred = lambda (key, item): not key in to_throw_out
-#        auto_dict = dict (filter (pred, auto_dict.iteritems()))
-#
-#    pred = lambda (key, item): item.accepting
-#    auto_dict = dict (filter (pred, auto_dict.iteritems()))
-#    
-#    return not not auto_dict
+    def parse (self, regexp):
+        """ Parses the giver regular expression and creates an NFA. """
+        self.gtr = getter (regexp)
+        self.stack = []
+        self.handle_or ()
+        if not self.stack:
+            raise Exception ('Empty regexp!')
+        self.stack[0].add_next_state (done_nfa ())
+        return self.stack[0]
 
 
 # Class for Determinate Finate Automaton merge
@@ -414,10 +405,11 @@ class node_dfa_m (object):
             new_list = state_list[0].state_list
             new_list.append(state_list[1])
             self.state_list = new_list
+        elif state_list[0] is None:
+            self.state_list = [state_list[1]]
         else:
             self.state_list = state_list
         self.accepting_id_list = []
-
     
     def __repr__ (self):
         state_list = map(lambda (x,y): x + ' -> ' + repr(y.state_list), self.paths.iteritems())
@@ -437,6 +429,7 @@ def add_to_state_list_merge (state_list, merge_automata):
         raise Exception ("Duplicate state found during merge")
     return state
 
+
 def merge_paths (merge_list):
     rv = collections.defaultdict(list)
     for merge in merge_list:
@@ -444,10 +437,10 @@ def merge_paths (merge_list):
             rv[k].append(v)
     return rv
 
+
 def merge (automata_list):
-    merge0 = automata_list[0]
-    
-    for merge1 in automata_list[1:]:
+    merge0 = [None]
+    for merge1 in automata_list[0:]:
         first_state = node_dfa_m([merge0[0], merge1[0]])
         new_automata = [first_state]
         for state in new_automata:
@@ -456,6 +449,40 @@ def merge (automata_list):
                 state.paths[symbol] = add_to_state_list_merge (state_list, new_automata)
         merge0 = new_automata
     return new_automata
+
+
+""" ==== Determinate automaton execution logic ==== """
+
+def execute (string, regexp_list):
+    start_time = time.time()
+    prs = parser ()
+    automata_list = [det(prs.parse(x), ''.join([c for c in set(x) if c not in '*|()'])) for x in regexp_list]
+    for i in xrange (len (automata_list)):
+        for state in automata_list[i]:
+            state.regexp_id = i
+    automata = merge (automata_list)
+    current_state = automata[0]
+    fail = False
+    for char in string:
+        if current_state.paths.has_key (char):
+            current_state = current_state.paths.get (char)
+        else:
+            fail = True
+            break
+    if fail:
+        print "Nothing accepted."
+#        return False
+        return (False, time.time() - start_time)
+    else:
+        accepted = filter(lambda x: x.accepting, current_state.state_list)
+        if not accepted:
+            print "Nothing accepted."
+#            return False
+            return (False, time.time() - start_time)
+        else:
+            print "Accepted", regexp_list[accepted[0].regexp_id]
+#            return True
+            return (True, time.time() - start_time)
 
 
 # vim: set ts=4 sw=4 sts=4 et
--- test-nfa.py
+++ test-nfa.py
@@ -1,56 +1,19 @@
 from nfa import *
 
-letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-# a(a|b)x*
-reg0 = char_nfa ('a') \
-       .add_next_state (or_nfa (char_nfa ('a'), char_nfa ('b'))) \
-       .add_next_state (asterix_nfa (char_nfa ('x'))) \
-       .add_next_state (done_nfa ())
 regexp0 = 'a(a|b)x*' 
 
-# (a|b)*
-reg1 = asterix_nfa(or_nfa(char_nfa('a'), char_nfa('b')))\
-       .add_next_state(done_nfa())
 regexp1 = '(a|b)*'
 
-# (ab*|bb)
-reg2 = or_nfa (char_nfa ('a').add_next_state (asterix_nfa(char_nfa('b'))),\
-               char_nfa ('b').add_next_state (char_nfa('b')))\
-       .add_next_state(done_nfa())
 regexp2 = '(ab*|bb)'
 
-#((c*)*)*
-reg3 = asterix_nfa(asterix_nfa(asterix_nfa(char_nfa('c')))).add_next_state(done_nfa())
 regexp3 = '((c*)*)*'
 
-#((c|a)|(b|xxb*))
-reg4 = or_nfa (or_nfa(char_nfa ('c'), char_nfa ('a')),\
-               or_nfa(char_nfa ('b'),\
-                      char_nfa ('x').add_next_state(char_nfa('x'))\
-                      .add_next_state(asterix_nfa(char_nfa('b')))))\
-       .add_next_state(done_nfa())
 regexp4 = '((c|a)|(b|xxb*))'
 
-#(((a|b)|c)|x)
-reg5 = or_nfa (or_nfa (or_nfa (char_nfa ('a'), char_nfa ('b')),\
-                       char_nfa('c')),\
-               char_nfa('x'))\
-       .add_next_state(done_nfa())
 regexp5 = '(((a|b)|c)|x)'
 
-# (x|(a|(b|c*)*)*)
-reg6 = or_nfa (char_nfa ("x"),\
-       asterix_nfa (or_nfa (char_nfa ('a'), \
-                            asterix_nfa (or_nfa (char_nfa ('b'),\
-                                                 asterix_nfa (char_nfa ('c')))))) \
-       ).add_next_state (done_nfa ())
 regexp6 = '(x|(a|(b|c*)*)*)'
 
-# x*(a|c)
-reg7 = asterix_nfa (char_nfa('x'))\
-       .add_next_state (or_nfa (char_nfa('a'), char_nfa ('c')))\
-       .add_next_state (done_nfa ())
 regexp7 = 'x*(a|c)'
 
 regexp8 = 'abb|aab'
@@ -59,16 +22,21 @@ regexp9 = '(a|b)*abb'
 
 regexp10 = '(ab|a)*'
 
-regexp_list = [regexp0, regexp1, regexp2, regexp3, regexp4, regexp5, regexp6, regexp7, regexp8, regexp9]
-execute ("aaabb", regexp_list)
+time = [0,0,0,0,0,0,0,0,0,0,0,0]
 
-#reg = regexp7
-#print reg
-#f = parse (reg)
-#for dfa in det(f, letters):
-#    if dfa is not None:
-#        print dfa
-#        for p in dfa.paths:
-#            print "\t", p, "->", dfa.paths[p]
+regexp_list = [regexp0, regexp1, regexp2, regexp3, regexp4, regexp5, regexp6, regexp7, regexp8, regexp9]
+(a, time[0]) = execute ("abb", regexp_list)
+(a, time[1]) = execute ("ccc", regexp_list)
+(a, time[2]) = execute ("abcbcbcbcbcbcxxxxxx", regexp_list)
+(a, time[3]) = execute ("aaaaaaad", regexp_list)
+(a, time[4]) = execute ("abcbbbbbbb", regexp_list)
+(a, time[5]) = execute ("xxxxxxc", regexp_list)
+(a, time[6]) = execute ("aabbabb", regexp_list)
+(a, time[7]) = execute ("xxbbbbb", regexp_list)
+(a, time[8]) = execute ("ab", regexp_list)
+(a, time[9]) = execute ("cx", regexp_list)
+
+time_all = sum(time)
+print "Execution time", time_all
 
 # vim: set ts=4 sw=4 sts=4 et
